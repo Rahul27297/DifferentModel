436aa2176c28ca567418d78def9d24ea4bd59a0c
Rahul27297 <rahul.chaudhari27297@gmail.com>
2018-10-26 21:49:55 +0530

    Brought the notices to working. Changes as suggested by Kaivalya. The has been documented yet.

--- public/js/smartsociety_createnotice.js
+++ public/js/smartsociety_createnotice.js
@@ -1,116 +1,185 @@
 var storageref = firebase.storage();
 
-function smartSociety(){
+var smartSociety = function () {
     var saveNoticeBtn = document.getElementById("saveNoticeBtn");
-    saveNoticeBtn.addEventListener('click',this.saveNotice.bind(this));
+    var publishNoticeBtn = document.getElementById("publishNoticeBtn");
+    saveNoticeBtn.addEventListener('click', this.saveNotice.bind(this));
+    publishNoticeBtn.addEventListener('click', this.publishNotice.bind(this));
     console.log("hrllo");
 }
 
 
-smartSociety.prototype.saveNotice = function(){
+smartSociety.prototype.saveNotice = function () {
     var noticeSubject = document.getElementById("noticeSubject");
-    //var noticeContent = document.getElementById("noticeContent");
     var noticeFile = document.getElementById("noticeFile");
     var noticeSubjectValue = noticeSubject.value;
-    //var noticeContentValue = noticeContent.value;
     var noticeFileValue = noticeFile.value;
     var noticeTenants = document.getElementById("Tenants");
-    var noticeOwners = document.getElementById("Owners")
+    var noticeOwner = document.getElementById("Owners")
     var noticeOwnerValue = "false";
     var noticeTenantValue = "false";
+    var noticeId = "";
+    var xhttp = new XMLHttpRequest();
     //TODO : writing a separate subroutine for calculating timestamp
     //TODO : Implement Callback for firebase fileupload for failure case 
-    
-    if(noticeSubjectValue == ""){
+
+    if (noticeSubjectValue == "") {
         window.alert("Please Enter Subject");
         noticeSubject.focus();
         return false;
     }
 
-    if(noticeFileValue == ""){
+    if (noticeFileValue == "") {
         window.alert("Please Upload a notice file");
         noticeSubject.focus();
         return false;
     }
 
-    if(noticeOwners.checked == false && noticeTenants.checked == false ){
+    if (noticeOwner.checked == false && noticeTenants.checked == false) {
         window.alert("Please select read recipients");
         noticeSubject.focus();
-        return false;        
+        return false;
     }
 
-    if(noticeOwners.checked == true){
-        noticeOwnersValue = noticeOwners.value;
+    if (noticeOwner.checked == true) {
+        noticeOwnerValue = noticeOwner.value;
     }
 
-    if(noticeTenants.checked == true){
+    if (noticeTenants.checked == true) {
         noticeTenantValue = noticeTenants.value;
     }
 
-    if(noticeFileValue != ""){
-        var date = new Date();
-        //YYYYMMDDHHMMSS is the stamp used. If month is not NOV/DEC, 0 isnt prepend to the month number
-        var year = date.getFullYear();
-        var month = date.getMonth();
-        var day = date.getDate();
-        var hour = date.getHours();
-        var minute = date.getMinutes();
-        var second = date.getSeconds();
-        
-        if(month < 10){month = "0" + month.toString();}
-        if(day < 10){day = "0" + day.toString();}
-        if(hour < 10){hour = "0" + hour.toString();}
-        if(minute < 10){minute = "0" + minute.toString();}
-        if(second < 10){second = "0" + second.toString();}
-
-        var timestamp = year+month+day+hour+minute+second;
-        console.log(timestamp);
-        var noticeStorageRef = storageref.ref(timestamp);
-        var $ = jQuery;
-        var file_data = $('#noticeFile').prop('files')[0];
-        noticeStorageRef.put(file_data);
-    }
-    /*if(noticeContentValue == ""){
-        window.alert("Please Enter Content for Notice");
-        noticeContent.focus();
-        return false;
-    }*/
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET","https://us-central1-differentmodel-5b6b0.cloudfunctions.net/createNotice"+"?societyID=1"+"&subject="+noticeSubjectValue+"&datetime="+timestamp+"&tenant="+noticeTenantValue+"&owner="+noticeOwnerValue,true);
+    xhttp.open("GET", "https://us-central1-differentmodel-5b6b0.cloudfunctions.net/getNoticeID" + "?societyID=1", true);
     xhttp.send();
     console.log("here");
-    xhttp.onreadystatechange = function(){
-        if ( this.readyState == 4 && this.status == 200){
-            console.log(xhttp.responseText);
-
-           /* const memberInfo = JSON.parse(xhttp.responseText).member;
-            if(memberInfo!=null){
-                window.alert("Member with given email ID already exists. Select a different email ID");
-    
-            }
-            else{
-                console.log("in addition")
-
-                // adding member using create member cloud function
-                var xhttp2 = new XMLHttpRequest();
-                console.log("https://us-central1-differentmodel-5b6b0.cloudfunctions.net/createMember"+"?societyID=1"+"&name="+name+"&contact="+contact+"&flatno="+flatno+"&type="+type+"&email="+email);
-                xhttp2.open("POST","https://us-central1-differentmodel-5b6b0.cloudfunctions.net/createMember"+"?societyID=1"+"&name="+name+"&contact="+contact+"&flatno="+flatno+"&type="+type+"&email="+email,true);
-                xhttp2.send();
-                xhttp2.onreadystatechange = function(){
-                    if ( this.readyState == 4 && this.status == 200){
-                        window.alert("Member Successfully Added")
-                    }
-                }
-            }*/
-           
-
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            noticeId = xhttp.responseText;
+            console.log(noticeId);
+            var noticespath = "/notices/" + ((parseInt(noticeId) + 1).toString());
+            if (noticeFileValue != "") {
+                var noticeStorageRef = storageref.ref(noticespath);
+                console.log(noticespath);
+                var $ = jQuery;
+                var file_data = $('#noticeFile').prop('files')[0];
+                noticeStorageRef.put(file_data);
+            }//TODO change the noticeid cloud function to remove +1 used above
+            //return true;
         }
-    }
-    
+    };
+    var xhttp1 = new XMLHttpRequest();
+    xhttp1.open("GET", "https://us-central1-differentmodel-5b6b0.cloudfunctions.net/createNotice" + "?societyID=1" + "&subject=" + noticeSubjectValue + "&tenant=" + noticeTenantValue + "&owner=" + noticeOwnerValue + "&isPublished=false&datetime=NA", true);
+    xhttp1.send();
+    console.log("here");
+    xhttp1.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(xhttp1.responseText);
+            if(xhttp1.responseText == "success"){
+                window.alert("Notice Saved Successfully");
+            }
+        }
+    };
 }
 
+smartSociety.prototype.publishNotice = function () {
+    var noticeSubject = document.getElementById("noticeSubject");
+    var noticeFile = document.getElementById("noticeFile");
+    var noticeSubjectValue = noticeSubject.value;
+    var noticeFileValue = noticeFile.value;
+    var noticeTenants = document.getElementById("Tenants");
+    var noticeOwner = document.getElementById("Owners")
+    var noticeOwnerValue = "false";
+    var noticeTenantValue = "false";
+    var noticeId = "";
+    var xhttp = new XMLHttpRequest();
+    //TODO : writing a separate subroutine for calculating timestamp
+    //TODO : Implement Callback for firebase fileupload for failure case 
 
-window.onload = function(){
+    if (noticeSubjectValue == "") {
+        window.alert("Please Enter Subject");
+        noticeSubject.focus();
+        return false;
+    }
+
+    if (noticeFileValue == "") {
+        window.alert("Please Upload a notice file");
+        noticeSubject.focus();
+        return false;
+    }
+
+    if (noticeOwner.checked == false && noticeTenants.checked == false) {
+        window.alert("Please select read recipients");
+        noticeSubject.focus();
+        return false;
+    }
+
+    if (noticeOwner.checked == true) {
+        noticeOwnerValue = noticeOwner.value;
+    }
+
+    if (noticeTenants.checked == true) {
+        noticeTenantValue = noticeTenants.value;
+    }
+
+    xhttp.open("GET", "https://us-central1-differentmodel-5b6b0.cloudfunctions.net/getNoticeID" + "?societyID=1", true);
+    xhttp.send();
+    console.log("here");
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            noticeId = xhttp.responseText;
+            console.log(noticeId);
+            noticeId = ((parseInt(noticeId) + 1).toString());
+            var noticespath = "/notices/" + noticeId;
+            if (noticeFileValue != "") {
+                var noticeStorageRef = storageref.ref(noticespath);
+                console.log(noticespath);
+                var $ = jQuery;
+                var file_data = $('#noticeFile').prop('files')[0];
+                noticeStorageRef.put(file_data);
+            }//TODO change the noticeid cloud function to remove +1 used above
+            //return true;
+        }
+    };
+
+    var date = new Date();
+    //YYYYMMDDHHMMSS is the stamp used. If month is not NOV/DEC, 0 isnt prepend to the month number
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+
+    if (month < 10) { month = "0" + month.toString(); }
+    if (day < 10) { day = "0" + day.toString(); }
+    if (hour < 10) { hour = "0" + hour.toString(); }
+    if (minute < 10) { minute = "0" + minute.toString(); }
+    if (second < 10) { second = "0" + second.toString(); }
+
+    var timestamp = (year + month + day + hour + minute + second).toString();
+    console.log(timestamp);
+
+    var xhttp1 = new XMLHttpRequest();
+    xhttp1.open("GET", "https://us-central1-differentmodel-5b6b0.cloudfunctions.net/createNotice" + "?societyID=1&subject=" + noticeSubjectValue + "&tenant=" + noticeTenantValue + "&owner=" + noticeOwnerValue + "&isPublished=true&datetime=" + timestamp, true);
+    xhttp1.send();
+    console.log("here");
+    xhttp1.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(xhttp1.responseText);
+            if(xhttp1.responseText == "success"){
+                window.alert("Notice Published Successfully");
+            }
+            //return true;
+        }
+    };
+
+            
+            //TODO change the noticeid cloud function to remove +1 used above
+            //return true;
+}
+    
+
+
+window.onload = function () {
     window.smartSociety = new smartSociety();
 };
